9c99ba1c63e8e00d08d31c874b33bbcd2f5d9e3e
Hemani Sajjadali <sajjadali.hemani@scania.com>
2017-11-30 16:22:57 +0100

    hej

---
+++ readHTMLPage.py
@@ -0,0 +1,43 @@
+import urllib.request
+import requests
+import json
+from html.parser import HTMLParser
+from bs4 import BeautifulSoup
+from collections import namedtuple
+import time
+
+headers = {
+    'Accept': 'application/json',
+}
+url = "https://eurest.mashie.com/public/menu/sn%C3%A4ckviken/bad07c57?country=se"
+r = requests.get(url)
+
+soup = BeautifulSoup(r.text, 'html.parser')
+data = soup.script.get_text()
+
+data = data.lstrip()
+data = data.replace('var weekData = ', '')
+data = data.replace('new Date(1511737200000)', '"new Date(1511737200000)"')
+data = data.replace('new Date(1511823600000)', '"new Date(1511823600000)"')
+data = data.replace('new Date(1511910000000)', '"new Date(1511910000000)"')
+data = data.replace('new Date(1511996400000)', '"new Date(1511996400000)"')
+data = data.replace('new Date(1512082800000)', '"new Date(1512082800000)"')
+
+
+json = json.loads(data)
+#print (json)
+
+numberOfDays = len(json["Weeks"][0]["Days"])
+numberOfAlternatives = len(json["Weeks"][0]["Days"][0]["DayMenus"])
+
+def getMenu():
+    WeekMenu = namedtuple('WeekMenu', ['Menualternative', 'Menuname'])
+    _menu = []
+
+    for i in range(0, numberOfDays):
+        for j in range(0, numberOfAlternatives):
+            _menu.append(WeekMenu(json["Weeks"][0]["Days"][i]["DayMenus"][j]["DayMenuName"],
+                                  json["Weeks"][0]["Days"][i]["DayMenus"][j]["MenuAlternativeName"]))
+    #print (_menu)
+    return _menu
+#getMenu()
--- runserver.py
+++ runserver.py
@@ -4,6 +4,140 @@ This script runs the FlaskWebProject1 application using a development server.
 
 from os import environ
 from FlaskWebProject1 import app
+from flask import Flask, request
+from flask_restful import Resource, Api
+from flask import Response
+
+import readHTMLPage
+import readOasenHTMLPage
+import time
+
+app = Flask(__name__)
+api = Api(app)
+
+#Snäckviken starts here
+class getMenuToday(Resource):
+    def get(self):
+
+        menu = readHTMLPage.getMenu()
+        today = time.strftime("%A").lower()
+
+        if(today == 'monday' or today == 'måndag'):
+            return {'Monday': [menu[0], menu[1], menu[2], menu[3], menu[4], menu[5]]}
+
+        if(today == 'tuesday' or today == 'tisdag'):
+            return {'Tuesday': [menu[6], menu[7], menu[8], menu[9], menu[10], menu[11]]}
+
+        if(today == 'wednesday' or today == 'onsdag'):
+            return {'Wednesday': [menu[12], menu[13], menu[14], menu[15], menu[16], menu[17]]}
+
+        if(today == 'thursday' or today == 'torsdag'):
+            return {'Thursday': [menu[18], menu[19], menu[20], menu[21], menu[22], menu[23]]}
+
+        if(today == 'friday' or today == 'fredag'):
+            return {'Friday': [menu[24], menu[25], menu[26], menu[27], menu[28], menu[29]]}
+
+class getMenu(Resource):
+    def get(self):
+
+        menu = readHTMLPage.getMenu()
+
+        menuJson = {'Monday': [menu[0], menu[1], menu[2], menu[3], menu[4], menu[5]],
+                    'Tuesday': [menu[6], menu[7], menu[8], menu[9], menu[10], menu[11]],
+                    'Wednesday': [menu[12], menu[13], menu[14], menu[15], menu[16], menu[17]],
+                    'Thursday': [menu[18], menu[19], menu[20], menu[21], menu[22], menu[23]],
+                    'Friday': [menu[24], menu[25], menu[26], menu[27], menu[28], menu[29]]}
+        return menuJson
+
+
+class getMenuDay(Resource):
+    def get(self, day):       
+        
+        menu = readHTMLPage.getMenu()
+        day = day.lower()
+        
+        if day == 'monday' or day == 'måndag':
+            return {'Monday': [menu[0], menu[1], menu[2], menu[3], menu[4], menu[5]]}
+
+        elif day == 'tuesday' or day == 'tisdag':
+            return {'Tuesday': [menu[6], menu[7], menu[8], menu[9], menu[10], menu[11]]}
+
+        elif day == 'wednesday' or day == 'onsdag':
+            return {'Wednesday': [menu[12], menu[13], menu[14], menu[15], menu[16], menu[17]]}
+
+        elif day == 'thursday' or day == 'torsdag':
+            return {'Thursday': [menu[18], menu[19], menu[20], menu[21], menu[22], menu[23]]}
+
+        elif day == 'friday' or day == 'fredag':
+            return {'Friday': [menu[24], menu[25], menu[26], menu[27], menu[28], menu[29]]}
+
+        else: 
+            return ({'Message':'Bad request syntax, enter a valid workday'}, 400)
+
+#Oasen starts here
+class getOasenMenuToday(Resource):
+    def get(self):
+
+        menu = readOasenHTMLPage.getMenu()
+        today = time.strftime("%A").lower()
+
+        if(today == 'monday' or today == 'måndag'):
+            return {'Monday': [menu[0], menu[1], menu[2]]}
+
+        if(today == 'tuesday' or today == 'tisdag'):
+            return {'Tuesday': [menu[3], menu[4], menu[5]]}
+
+        if(today == 'wednesday' or today == 'onsdag'):
+            return {'Wednesday': [menu[6], menu[7], menu[8]]}
+
+        if(today == 'thursday' or today == 'torsdag'):
+            return {'Thursday': [menu[9], menu[10], menu[11]]}
+
+        if(today == 'friday' or today == 'fredag'):
+            return {'Friday': [menu[12], menu[13], menu[14]]}
+
+class getOasenMenu(Resource):
+    def get(self):
+
+        menu = readOasenHTMLPage.getMenu()
+
+        menuJson = {'Monday': [menu[0], menu[1], menu[2]],
+                    'Tuesday': [menu[3], menu[4], menu[5]],
+                    'Wednesday': [menu[6], menu[7], menu[8]],
+                    'Thursday': [menu[9], menu[10], menu[11]],
+                    'Friday': [menu[12], menu[13], menu[14]]}
+        return menuJson
+
+class getOasenMenuDay(Resource):
+    def get(self, day):       
+        
+        menu = readOasenHTMLPage.getMenu()
+        day = day.lower()
+        
+        if day == 'monday' or day == 'måndag':
+            return {'Monday': [menu[0], menu[1], menu[2]]}
+
+        elif day == 'tuesday' or day == 'tisdag':
+            return {'Tuesday': [menu[3], menu[4], menu[5]]}
+
+        elif day == 'wednesday' or day == 'onsdag':
+            return {'Wednesday': [menu[6], menu[7], menu[8]]}
+
+        elif day == 'thursday' or day == 'torsdag':
+            return {'Thursday': [menu[9], menu[10], menu[11]]}
+
+        elif day == 'friday' or day == 'fredag':
+            return {'Friday': [menu[12], menu[13], menu[14]]}
+
+        else: 
+            return ({'Message':'Bad request syntax, enter a valid workday'}, 400)
+
+api.add_resource(getMenu, '/menuSnackviken')
+api.add_resource(getMenuDay, '/menuSnackviken/<day>')
+api.add_resource(getMenuToday, '/menuSnackvikenToday')
+api.add_resource(getOasenMenu, '/menuOasen')
+api.add_resource(getOasenMenuDay, '/menuOasen/<day>')
+api.add_resource(getOasenMenuToday, '/menuOasenToday')
 
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
